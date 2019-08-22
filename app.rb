8e9988126aa0358338645cc501c1b08dec23f3e5
Emily Lillie <emilylillie3@gmail.com>
2019-08-22 20:42:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,6 +5,14 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Emily Lillie"
   end
+  
+  get '/hometown' do 
+    "My hometown is Washington"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is 99 Problems"
+  end 
 
   
 end
